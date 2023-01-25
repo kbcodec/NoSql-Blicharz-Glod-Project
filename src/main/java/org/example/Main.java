d84036c89ba4a8c18446012edac7e9f46d6bbdce
Kacper Głód <kacper01.glod@gmail.com>
2023-01-25 19:53:23 +0100

    Inhabitant Generator v2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,7 @@ package org.example;
 
 import org.example.collections.building.Buildings;
 import org.example.collections.district.Districts;
+import org.example.collections.inhabitant.InhabitantGenerator;
 import org.example.collections.inhabitant.Inhabitants;
 
 import java.util.List;
@@ -9,5 +10,7 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
         new MenuForm("Menu").setVisible(true);
+
+            InhabitantGenerator.Generator();
     }
 }
--- src/main/java/org/example/collections/inhabitant/InhabitantGenerator.java
+++ src/main/java/org/example/collections/inhabitant/InhabitantGenerator.java
@@ -21,41 +21,41 @@ import static org.bson.codecs.configuration.CodecRegistries.fromRegistries;
 
 public class InhabitantGenerator {
 
-    CodecProvider pojoCodecProvider = PojoCodecProvider.builder().automatic(true).build();
-    CodecRegistry pojoCodecRegistry = fromRegistries(getDefaultCodecRegistry(), fromProviders(pojoCodecProvider));
+    static public void Generator() {
+        CodecProvider pojoCodecProvider = PojoCodecProvider.builder().automatic(true).build();
+        CodecRegistry pojoCodecRegistry = fromRegistries(getDefaultCodecRegistry(), fromProviders(pojoCodecProvider));
 
-    MongoDatabase database = MongoDBConnection.connect(pojoCodecRegistry);
+        MongoDatabase database = MongoDBConnection.connect(pojoCodecRegistry);
 
-    //MongoClient mongoClient = MongoClients.create();
-    //MongoDatabase database = mongoClient.getDatabase("CityDB");
-    MongoCollection<Document> collection = database.getCollection("Inhabitants");
+        //MongoClient mongoClient = MongoClients.create();
+        //MongoDatabase database = mongoClient.getDatabase("CityDB");
+        MongoCollection<Document> collection = database.getCollection("Inhabitants");
 
-    ArrayList<String> firstNames = new ArrayList<>(Arrays.asList("Pavel", "Grenville", "Edvard", "Irene", "Percival", "Blinny", "Lea", "Mohandas"));
+        ArrayList<String> firstNames = new ArrayList<>(Arrays.asList("Pavel", "Grenville", "Edvard", "Irene", "Percival", "Blinny", "Lea", "Mohandas"));
 
-    ArrayList<String> lastNames = new ArrayList<>(Arrays.asList("Melluish", "Duffin", "Walter", "Bodycomb", "Ranshaw", "Gwillim", "Titmuss", "Linny"));
-    ArrayList<Integer> dateOfBirths = new ArrayList<>(Arrays.asList(1999, 2010, 2009, 1988, 1959, 1998, 1998, 1990));
-    ArrayList<String> genders = new ArrayList<>(Arrays.asList("Male", "Male", "Male", "Female", "Male", "Female", "Female", "Male"));
-    ArrayList<Integer> buildingIds = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8));
-    ArrayList<Integer> districtIds = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8));
-    ArrayList<String> educations = new ArrayList<>(Arrays.asList("Dr", "Mrs", "Dr", "Rev", "Dr", "Honorable", "Honorable", "Mr"));
-    ArrayList<String> professions = new ArrayList<>(Arrays.asList("Senior Financial Analyst", "Software Consultant", "VP Marketing", "Office Assistant IV", "Systems Administrator III", "Programmer IV", "Recruiting Manager", "Research Assistant IV"));
+        ArrayList<String> lastNames = new ArrayList<>(Arrays.asList("Melluish", "Duffin", "Walter", "Bodycomb", "Ranshaw", "Gwillim", "Titmuss", "Linny"));
+        ArrayList<Integer> dateOfBirths = new ArrayList<>(Arrays.asList(1999, 2010, 2009, 1988, 1959, 1998, 1998, 1990));
+        ArrayList<String> genders = new ArrayList<>(Arrays.asList("Male", "Male", "Male", "Female", "Male", "Female", "Female", "Male"));
+        ArrayList<Integer> buildingIds = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8));
+        ArrayList<Integer> districtIds = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8));
+        ArrayList<String> educations = new ArrayList<>(Arrays.asList("Dr", "Mrs", "Dr", "Rev", "Dr", "Honorable", "Honorable", "Mr"));
+        ArrayList<String> professions = new ArrayList<>(Arrays.asList("Senior Financial Analyst", "Software Consultant", "VP Marketing", "Office Assistant IV", "Systems Administrator III", "Programmer IV", "Recruiting Manager", "Research Assistant IV"));
 
 
+        List<Document> inhabitants = new ArrayList<>();
+        for (int i = 0; i < firstNames.size(); i++) {
+            inhabitants.add(new Document("inhabitantId", i + 1)
+                    .append("personalInfo.firstName", firstNames.get(i))
+                    .append("personalInfo.lastName", lastNames.get(i))
+                    .append("personalInfo.dateOfBirth", dateOfBirths.get(i))
+                    .append("personalInfo.gender", genders.get(i))
+                    .append("buildingId", buildingIds.get(i))
+                    .append("districtId", districtIds.get(i))
+                    .append("education", educations.get(i))
+                    .append("profession", professions.get(i)));
+        }
 
-//    List<Document> inhabitants = new ArrayList<>();
-//        for (int i = 0; i < firstNames.length; i++) {
-//        inhabitants.add(new Document("inhabitantId", i + 1)
-//                .append("personalInfo.firstName", firstNames.get(i))
-//                .append("personalInfo.lastName", lastNames.get(i))
-//                .append("personalInfo.dateOfBirth", dateOfBirths.get(i))
-//                .append("personalInfo.gender", genders.get(i))
-//                .append("buildingId", buildingIds.get(i))
-//                .append("districtId", districtIds.get(i))
-//                .append("education", educations.get(i))
-//                .append("profession", professions.get(i)));
-//    }
-//
-//        database.getCollection("Inhabitants").insertMany(inhabitants);
+        database.getCollection("Inhabitants").insertMany(inhabitants);
+    }
 }
-
 
