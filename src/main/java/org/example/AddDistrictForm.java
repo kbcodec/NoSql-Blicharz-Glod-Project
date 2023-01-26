def832efe2c1c16c74a93d1ccbb26c785f335cf3
Konrad Blicharz <konradcodec@gmail.com>
2023-01-26 02:15:02 +0100

    adding district form scatch

--- src/main/java/org/example/AddDistrictForm.java
+++ src/main/java/org/example/AddDistrictForm.java
@@ -4,4 +4,19 @@ import javax.swing.*;
 
 public class AddDistrictForm {
     private JPanel MainPanel;
+    private JTextField nameTextField;
+    private JTextField areaTextField;
+    private JTextField utilityBuildingsTextField;
+    private JTextField residentialBuildingsTextField;
+    private JTextField industrialBuildingsTextField;
+    private JComboBox hasParkCb;
+    private JTextField parkNameTextField;
+    private JTextField fountainsNumberTextField;
+    private JTextField benchesNumberTextField;
+    private JTextField entrancesNumberTextField;
+    private JTextField parkAreaTextField;
+    private JTextField busLinesTextField;
+    private JTextField tramLinesTextField;
+    private JTextField inhabitantsNumberTextField;
+    private JButton updateDataButton;
 }
